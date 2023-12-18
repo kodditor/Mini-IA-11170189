2028e8edbf6f1e055607b81343a51a500bbc93f9
kodditor <kobbyowusudarko@gmail.com>
2023-12-18 09:17:11 +0000

    Feat: 'Update Vitals'

--- server.js
+++ server.js
@@ -29,15 +29,41 @@ app.get('/patient/:patientID', async (req, res) => {
 
 })
 
+
 app.post('/patient', async (req, res) => {
     const patientObject = req.body
+    patientObject.vitals = {
+        temperature: 0,
+        bloodPressure: 0,
+        pulse: 0,
+        SPO2: 0,
+    }
+    
+    patientObject.createdAt = new Date()
+
     const model = getModel()
     const newPatient = await model.insertMany(patientObject)
+    
     if(newPatient){
         res.status(200).json(newPatient)
     }
     else {
         res.status(500).send('Error: Could not create new patient object')
+    }
+})
+
+app.post('/patient/:patientID/vitals', async (req, res) =>{
+    const patientID = req.params.patientID
+    const vitals = req.body
+    const model = getModel()
+    const updatedPatient = await model.updateOne({'patientID': patientID}, { $set: {
+        vitals: vitals
+    }})
+
+    if(updatedPatient) {
+        res.status(200).json(updatedPatient)
+    } else {
+        res.status(500).send(`Error: Could not update patient with id: ${patientID}`)
     }
 })
 
