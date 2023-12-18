af2399ab3a6774b0bbaea5ebc1a123c001120c69
kodditor <kobbyowusudarko@gmail.com>
2023-12-18 08:49:48 +0000

    feat: Added 'List Patients'

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import connectDB from './db'
+import { connectDB, getModel } from './db.js'
 
 const app  = express()
 const PORT = 5000
@@ -9,6 +9,11 @@ app.get('/', (req, res) => {
     res.status(200).send('Ping!\nThis is the Electronic Medical Record System for UGMC')
 })
 
+app.get('/patients', async (req,res) => {
+    const model = getModel()
+    const allPatients = await model.find()
+    res.status(200).send(allPatients)
+})
 
 app.listen(PORT, ()=>{
     console.log(`Started server at http://localhost:${PORT}`)
