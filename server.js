f6fd2c68ca8310f75972fa42e0d783ddfe3cd98a
kodditor <kobbyowusudarko@gmail.com>
2023-12-18 09:00:49 +0000

    Feat: 'Create Patient'

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app  = express()
 const PORT = 5000
 connectDB()
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.status(200).send('Ping!\nThis is the Electronic Medical Record System for UGMC')
 })
@@ -14,6 +16,19 @@ app.get('/patients', async (req,res) => {
     const allPatients = await model.find()
     res.status(200).send(allPatients)
 })
+
+app.post('/patient', async (req, res) => {
+    const patientObject = req.body
+    const model = getModel()
+    const newPatient = await model.insertMany(patientObject)
+    if(newPatient){
+        res.status(200).json(newPatient)
+    }
+    else {
+        res.status(500).send('Error: Could not create new patient object')
+    }
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`Started server at http://localhost:${PORT}`)
