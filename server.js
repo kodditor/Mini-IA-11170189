1a612a05deb97e033f0d34b538415855bcacf156
kodditor <kobbyowusudarko@gmail.com>
2023-12-18 08:43:03 +0000

    Added DB connection to server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from 'express'
+import connectDB from './db'
 
 const app  = express()
 const PORT = 5000
+connectDB()
 
 app.get('/', (req, res) => {
     res.status(200).send('Ping!\nThis is the Electronic Medical Record System for UGMC')
 })
+
 
 app.listen(PORT, ()=>{
     console.log(`Started server at http://localhost:${PORT}`)
